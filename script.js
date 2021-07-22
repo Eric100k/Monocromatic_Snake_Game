1da3a6f03b6620e33fe574baa205053b77a76d9d
Eric100k <ericpatricwest@hotmail.com>
2021-07-21 21:14:07 -0300

    Background

---
+++ script.js
@@ -0,0 +1,21 @@
+let canvas = document.getElementById("snake"); //elemento que roda o jogo
+let context = canvas.getContext("2d");
+let box = 36;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarBG() {
+    context.fillStyle = '#242424';
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarcobrinha() {
+    for(i = 0;i < snake.length; i++){
+
+    }
+}
+
+criarBG();
