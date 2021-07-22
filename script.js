981a3f66318074fb58bea7c1a4f0589d575bce16
Eric100k <ericpatricwest@hotmail.com>
2021-07-21 21:37:35 -0300

    Movimento da bichinha

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = 'right';
+
 function criarBG() {
     context.fillStyle = '#242424';
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -15,10 +17,30 @@ function criarBG() {
 function criarcobrinha() {
     for(i = 0; i < snake.length; i++){
         context.fillStyle = "darkgray";
-        context.fillRect(snake[i].x, snake[i].y, box, box)
-
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarcobrinha();
+function StartGame() {
+    criarBG();
+    criarcobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeY -= box;
+    if(direction == 'up') snakeY += box;
+    if(direction == 'down') snakeX -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(StartGame, 100);
